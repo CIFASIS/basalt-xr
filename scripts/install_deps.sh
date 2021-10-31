4895eaedc0b161fccaed5b57fac43b95472379a1
Jan Schmidt <jan@centricular.com>
2021-11-01 01:15:28 +1100

    Add a script to install deps on Fedora

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -14,5 +14,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 if [[ "$OSTYPE" == "darwin"* ]]; then
 ${DIR}/install_mac_os_deps.sh
 else
-${DIR}/install_ubuntu_deps.sh
+	DISTRO=$( awk -F= '/^ID/{print $2}' /etc/os-release )
+	if [ "$DISTRO" == "fedora" ]; then
+		${DIR}/install_fedora_deps.sh
+	else
+		${DIR}/install_ubuntu_deps.sh
+	fi
 fi
---
+++ scripts/install_fedora_deps.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo dnf install -y gcc g++ cmake git tbb-devel eigen3-devel glew-devel ccache libjpeg-turbo-devel libpng-devel lz4-devel bzip2-devel boost-regex boost-filesystem boost-date-time boost-program-options gtest-devel opencv-devel
